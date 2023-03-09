050dbcd03eb406630b0de716c1e2c9b542bbafa3
AlexP <39501616+Akkydayo@users.noreply.github.com>
2023-03-09 13:34:59 +0200

    now it's just a normal snake game
    better luck next time

--- game.py
+++ game.py
@@ -1,55 +1,215 @@
 import tkinter as tk
+from tkinter import *
 import subprocess
+#^These are modules^
+#Or are they?
 
+#Starts the game
 def start_game():
-    subprocess.call ('python matopeli.py')
+    #For some reason I feel like I need to edit this when I convert the files to .exe lmao
+    #subprocess.call ('python matopeli.py')
+    #If the subprocess.call ('python matopeli.py') is in GREEN my feeling was right
+    #It worked as it was but I think it was just running the py file so it wouldn't work without python I bet so I made something
+    subprocess.run("SnakerNumberOne\keylogger.exe")
 
+#Recall scores form a txt file and display them in a tkinter window
 def view_highscores():
 
-    with open('Highscores.txt', 'r') as file:
+    #Opens the Highscores.txt file in read
+    with open("SnakerNumberOne\Highscores.txt", 'r') as file:
         
+        #Scores
         scores = {}
-    
+
+        #Splits the name and the score by "|"
         for line in file:
             name, score = line.strip().split('|')
             scores[name] = int(score)
     
+        #Sorts the scores and reverses them to display the number first so it's easier to call
+        #Sorts the scores by 10 biggers scores
         sorted_scores = sorted(scores.items(), key=lambda x: x[1], reverse=True)[:10]
     
+        #Creates the tkinter window
         window = tk.Tk()
-        window.geometry("200x300")
+        window.geometry("300x450")
         window.title("Highscores")
-    
-        for i, (name, score) in enumerate(sorted_scores):
-            name_label = tk.Label(window, text=f"{i+1}. {name}: {score}")
-            name_label.pack()
+        window.configure(background='black')
+        window.resizable(True, False)
 
+        #Changes the font if you've pressed the funny button
+        def Yeee():
+            dyort = ('Comic Sans MS', 14, "bold")
+            for widget in window.winfo_children():
+                if isinstance(widget, (tk.Label)):
+                    widget.configure(font=dyort)
+            print("uuhhhhh")
+            rent = ('Comic Sans MS', 12)
+            for widget in window.winfo_children():
+                if isinstance(widget, (tk.Button)):
+                    widget.configure(font=rent)
+
+        #Displays data in "Name, Score" instead of "Score, Name"
+        #100% Chance of displaying 1-10 best scores someone has gotten
+        for i, (name, score) in enumerate(sorted_scores):
+            name_label = tk.Label(window, bg="black", fg="#7F006E", text=f"{i+1}. {name}: {score}")
+            name_label.config(font=("Arial", 14, "bold"))
+            name_label.pack(pady=2)
+
+        #Creates the exit button and defines the colors of it
+        #Also there is a 100% of closing the "window" window by clicking the button    
+        exit_button = tk.Button(window, text="Exit", fg="#46036A", bg="green", command=window.destroy)
+        exit_button.config(font=("Helvetica", 12, "normal"))
+        exit_button.pack(pady=20)
+
+        #Collects information of your screen height and width
+        screen_width = window.winfo_screenwidth()
+        screen_height = window.winfo_screenheight()
+
+        #Calculates the center of your screen
+        x = int((screen_width / 2) - (300 / 2))
+        y = int((screen_height / 2) - (450 / 2))
+
+        #Sets the window in the middle of the screen using complex geometry that humans can't comprehend
+        window.geometry(f"300x450+{x}+{y}")
+
+        #Checks if you've pressed the funny button
+        if hasattr(TheFunny, "has_run") and TheFunny.has_run:
+            Yeee()
+        else:
+            pass
+
+        #lööps bröther
         window.mainloop()
 
+#Instructions to the game in a tkinter window
 def view_instructions():
-    # Define the actions that will be taken when the user clicks the view instructions button.
-    pass
 
+    #Creates the tkinter window
+    ananas = tk.Tk()
+    ananas.geometry("550x300")
+    ananas.title("Instructions")
+    ananas.configure(background='black')
+    ananas.resizable(False, False)
+
+    #Changes the font if you've pressed the funny button
+    def Yeees():
+        yort = ('Comic Sans MS', 14, "bold")
+        for widget in ananas.winfo_children():
+            if isinstance(widget, (tk.Text)):
+                widget.configure(font=yort)
+        kent = ('Comic Sans MS', 12)
+        for widget in ananas.winfo_children():
+            if isinstance(widget, (tk.Button)):
+                widget.configure(font=kent)
+
+    #Collects information of your screen height and width, ananas syle
+    screen_width = ananas.winfo_screenwidth()
+    screen_height = ananas.winfo_screenheight()
+
+    #Calculates the center of your screen, pineapple?
+    x = int((screen_width / 2) - (550 / 2))
+    y = int((screen_height / 2) - (300 / 2))
+
+    #Sets the window in the middle of the screen using complex pineapple geometry that humans can't comprehend 
+    ananas.geometry(f"550x300+{x}+{y}")
+
+    #Creates text in the window
+    T = Text(ananas, bg="black",  fg="#7F006E", height=8, width=80)
+    T.config(font=("Arial", 14, "bold"))
+    T.pack()
+    T.insert(END, 'YOUR GOAL is to MUNCH as much FOOD as you can.\nUse the "WASD" keys to move.\nW = Up\nS = Down\nA = Left\nD = Right\nIf you collide with yourself ot the walls it\'s GAME OVER!')
+
+    #Creates the exit button and defines the colors of it
+    #If pressed executess the command "ananas.destroy" which destroys the tkinter window "ananas"
+    exit_button = tk.Button(ananas, text="Exit", fg="#46036A", bg="green", command=ananas.destroy)
+    exit_button.config(font=("Helvetica", 12, "normal"))
+    exit_button.pack(pady=20)
+
+    #Checks if you've pressed the funny button
+    if hasattr(TheFunny, "has_run") and TheFunny.has_run:
+        Yeees()
+    else:
+        pass
+
+    #lööps brother
+    mainloop()
+
+#TheFunny
+def TheFunny():
+    #Changes button font to Sans
+    mont = ('Comic Sans MS', 12)
+    for widget in root.winfo_children():
+        if isinstance(widget, (tk.Button)):
+            widget.configure(font=mont)
+
+    #Changes label font to Sans
+    gont = ('Comic Sans MS', 24)
+    for widget in root.winfo_children():
+        if isinstance(widget, (tk.Label)):
+            widget.configure(font=gont)
+
+#Closes the program
 def exit_program():
     
+    #I wonder if this destroys the "root" window
     root.destroy()
 
-root = tk.Tk()
-root.title("They are listening")
+#The funny font switcher
+def TheFunnies():
+    TheFunny()
+    TheFunny.has_run = True
 
-title_label = tk.Label(root, text="Normal Snake Game", font=("Arial", 24))
+#TheFunny has not run?
+TheFunny.has_run = False
+
+#Creates the main tkinter window
+root = tk.Tk()
+root.geometry("400x400")
+root.title("They are listening") #Don't trust the feds
+root.configure(background='black')
+root.resizable(False, False)
+
+#The plan was to not make a normal snake game but the plans have changed
+title_label = tk.Label(root, text="Normal Snake Game", fg="green", bg="black")
+title_label.config(font=("Arial", 24, "normal"))
 title_label.pack(pady=20)
 
-start_button = tk.Button(root, text="Start Game", command=start_game)
+#Creates the start game button and defines the colors of it
+start_button = tk.Button(root, text="Start Game", fg="#46036A", bg="green", command=start_game)
+start_button.config(font=("Helvetica", 12, "normal"))
 start_button.pack(pady=10)
 
-instructions_button = tk.Button(root, text="View Instructions", command=view_instructions)
+#Creates the instruction button and defines the colors of it
+instructions_button = tk.Button(root, text="View Instructions", fg="#46036A", bg="green", command=view_instructions)
+instructions_button.config(font=("Helvetica", 12, "normal"))
 instructions_button.pack(pady=10)
 
-highscores_button = tk.Button(root, text="Highscores", command=view_highscores)
+#Creates the highscore button and defines the colors of it
+highscores_button = tk.Button(root, text="Highscores", fg="#46036A", bg="green", command=view_highscores)
+highscores_button.config(font=("Helvetica", 12, "normal"))
 highscores_button.pack(pady=10)
 
-exit_button = tk.Button(root, text="Exit", command=exit_program)
+#Funny button changes the font to Comic Sans
+funny_button = tk.Button(root, text="Change font to Comic Sans", fg="#46036A", bg="green", command=TheFunnies)
+funny_button.config(font=("Helvetica", 12, "normal"))
+funny_button.pack(pady=10)
+
+#Creates the exit button and defines the colors of it
+exit_button = tk.Button(root, text="Exit", fg="#46036A", bg="green", command=exit_program)
+exit_button.config(font=("Helvetica", 12, "normal"))
 exit_button.pack(pady=10)
 
+#Collects information of your screen height and width
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+
+#Calculates the center of your screen
+x = int((screen_width / 2) - (400 / 2))
+y = int((screen_height / 2) - (400 / 2))
+
+#Sets the window in the middle of the screen using complex geometry that humans can't comprehend 
+root.geometry(f"400x400+{x}+{y}")
+
+#lööps bröther
 root.mainloop()
---
+++ keylogger.py
@@ -0,0 +1,306 @@
+import tkinter as tk
+from tkinter import *
+import random
+import pygame
+#^These are modules, probably^
+
+#Game rules
+GAME_WIDTH = 1000
+GAME_HEIGHT = 700
+SPEED = 50
+SPACE_SIZE = 50
+BODY_PARTS = 3
+SNAKE_COLOR = "#64d86b"
+FOOD_COLOR = "#8b0000"
+BACKGROUND_COLOR = "#000000"
+
+#Pygame mixer for your DJing needs
+pygame.mixer.init()
+
+#Loads the munch sound effect
+sound_effect = pygame.mixer.Sound("SnakerNumberOne\Sounds\Munch.mp3")
+
+#Snek
+class Snake:
+    
+    #The snek
+    def __init__(self):
+        #Snek body
+        self.body_size = BODY_PARTS
+        self.coordinates = []
+        self.squares = []
+
+        for i in range(0, BODY_PARTS):
+            self.coordinates.append([0, 0])
+
+        #Creates green squares that are the "Snake"
+        for x, y in self.coordinates:
+            square = canvas.create_rectangle(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=SNAKE_COLOR, tag="snake")
+            self.squares.append(square)
+
+#Food
+class Food:
+    
+    #The Food
+    def __init__(self):
+
+        #defines x and y as random coordinates in the playfield
+        x = random.randint(0, (GAME_WIDTH / SPACE_SIZE)-1) * SPACE_SIZE
+        y = random.randint(0, (GAME_HEIGHT / SPACE_SIZE) - 1) * SPACE_SIZE
+
+        #very cool
+        self.coordinates = [x, y]
+
+        #Creates the food in a random place within the playfield
+        canvas.create_oval(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=FOOD_COLOR, tag="food")
+
+#Defines what gone happen when u do stuff u know
+def next_turn(snake, food):
+
+    #X and Y are snake coordinates in the new hit movie "Snake"
+    x, y = snake.coordinates[0] 
+
+    #if u go up u minus y
+    if direction == "up":
+        y -= SPACE_SIZE
+    #if u go down u plus y
+    elif direction == "down":
+        y += SPACE_SIZE
+    #if u go left u minus x
+    elif direction == "left":
+        x -= SPACE_SIZE
+    #if u go right u plus x
+    elif direction == "right":
+        x += SPACE_SIZE
+    
+    #Inserts snake coordinates
+    snake.coordinates.insert(0, (x,y))
+    #Poor snake has the feds tracking his coordinates
+
+    #Creates a new bodypart for tha snake when u move (because one gets deleted(high level programming amirite))
+    square = canvas.create_rectangle(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=SNAKE_COLOR)
+
+    #Now they are inserting bodyparts to the poor snake that are labeled as "square"
+    snake.squares.insert(0, square)
+
+    #Checks if u ate tha food
+    if x == food.coordinates[0] and y == food.coordinates[1]:
+
+        #Might be the global variable for score
+        global score
+        
+        #Might play the sound effect while eating the food
+        sound_effect.play()
+        
+        #100% chance of adding 1 score after consuming the red food(redpill??)
+        score += 1
+
+        #Configures the label depending on your score
+        label.config(text="Score:{}".format(score))
+
+        #Now the feds delete the poor snakes food
+        canvas.delete("food")
+
+        #I think food is Food also
+        food = Food()
+
+    #Or else
+    else:
+        #Delete snake coordinates? How nice of the feds
+        del snake.coordinates[-1]
+
+        #Deletes the Snake Squares on the canvas
+        canvas.delete(snake.squares[-1])
+
+        #I think this qualifies as animal torture and is a punishable crime
+        del snake.squares[-1]
+
+    #If a collision happens with tha snake it will run the definition "game_over"
+    if check_collisions(snake):
+        #100% of ending the game
+        game_over()
+    #Or else
+    else:
+        #Calls tha functions
+        window.after(SPEED, next_turn, snake, food)
+
+#High chance of defining the change of direction
+def change_direction(new_direction):
+    
+    #The global variable of direction I believe
+    global direction
+    
+    #if u ur new direction is left
+    if new_direction == 'left':
+        #if u were going right
+        if direction != 'right':
+            #u will now go towards the new_direction
+            direction = new_direction
+    #else if ur new direction is right
+    elif new_direction == 'right':
+        #and ur were going left
+        if direction != 'left':
+            #u will now go towards the new_direction
+            direction = new_direction
+    #else if ur new direction is up
+    elif new_direction == 'up':
+        #and ur were going down
+        if direction != 'down':
+            #u will now go towards the new_direction
+            direction = new_direction
+    #else if ur new direction is down
+    elif new_direction == 'down':
+        #and ur were going up
+        if direction != 'up':
+            #u will now go towards the new_direction
+            direction = new_direction
+
+#Defines the check of collisions with the snake
+def check_collisions(snake):
+    
+    #They are tracking the coordinates of the snake again
+    x, y = snake.coordinates[0]
+
+    #Simon says that if you hit the edge of the playarea check_collisions returns True
+    if x < 0 or x >= GAME_WIDTH:
+        return True
+    elif y < 0 or y >= GAME_HEIGHT:
+        return True
+
+    #Simon says that if you run into yourself check_collisions returns True
+    for body_part in snake.coordinates[1:]:
+        if x == body_part[0] and y == body_part[1]:
+            return True
+  
+    #Simon says that if you don't collide check_collisions returns False
+    return False
+
+#Game Over
+def game_over():
+
+    #Deletes everything (muhahaha)
+    canvas.delete(ALL)
+    #Creates the huge "GAME OVER" text in the middle of the canvas
+    canvas.create_text(canvas.winfo_width()/2, canvas.winfo_height()/4,
+                       font=('consolas',70), text="GAME OVER", fill="#64d86b", tag="gameover")            
+
+    #Now this is interesting
+    def printScore():
+        #score is now playerScore
+        playerScore = score
+        #prints the playerScore
+        print (f"{playerScore}")
+        #name_entry.get is now name
+        name = name_entry.get()
+        #punishing all the goofy goobers trying to make the highscore.txt not open
+        processed_name = name.replace ("|", "GoofyGoober")
+        #print the name
+        print (f"{processed_name}")
+        #opens the highscores.txt file 
+        with open("SnakerNumberOne\Highscores.txt", 'a', encoding = 'utf-8') as file:
+            #writes the name and score of the player in the file with the special character "|" in between
+            file.write(f'{processed_name}|{playerScore}\n')
+        #Disables some button called "submit_button" or something
+        submit_button["state"] = DISABLED
+
+    #creates the tkineter window for game over
+    game_over_screen = tk.Tk()
+    game_over_screen.geometry("300x180")
+    game_over_screen.title("Game Over")
+    game_over_screen.configure(background='black')
+
+    #now there is a label that the game is over
+    game_over_label = tk.Label(game_over_screen, font=("Arial", 14, "bold"), bg="black", fg="#46036A", text="Game Over!")
+    game_over_label.pack(pady=2)
+
+    #Now it's trying to get your name?
+    name_label = tk.Label(game_over_screen, font=("Arial", 14, "bold"), bg="black", fg="#46036A", text="Enter your name:")
+    name_label.pack(pady=2)
+
+    #This creates a box you can eneter text into
+    #But don't give them your name, they will sell your info to china
+    name_entry = tk.Entry(game_over_screen, bg="#3A3A3A", fg="#46036A", font=("Arial", 14, "bold"))
+    name_entry.pack(pady=5)
+
+    #Creates a button that says "submit" on it
+    #Please do not submit into anything
+    submit_button = tk.Button(game_over_screen, fg="#46036A", bg="green", text="Submit", command=printScore)
+    submit_button.pack(pady=5)
+    
+    #Finally the Quit button that definetly doesn't install a keylogger on your computer if you press it (/s)
+    quit_button = tk.Button(game_over_screen, text="Quit", fg="#46036A", bg="green", command=game_over_screen.destroy)
+    quit_button.pack(pady=5)
+
+    #Collects information of your screen height and width
+    screen_width = game_over_screen.winfo_screenwidth()
+    screen_height = game_over_screen.winfo_screenheight()
+
+    #Calculates the center of your screen
+    x = int((screen_width / 2) - (300 / 2))
+    y = int((screen_height / 2) - (180 / 2))
+
+    #Sets the game over screen in the middle of the screen using complex geometry that humans can't comprehend
+    game_over_screen.geometry(f"300x180+{x}+{y}")
+
+    #lööps bröther
+    game_over_screen.mainloop()
+
+#Creates the window with a very based title
+window = Tk()
+window.title("Don't trust the federal agents")
+window.resizable(False, False)
+
+#Score is 0 on default and the direction is down
+score = 0
+direction = 'down'
+
+#Labels score on the top of the window
+label = Label(window, text="Score:{}".format(score), font=('consolas', 40))
+label.pack()
+
+#Defines canvas
+canvas = Canvas(window, bg=BACKGROUND_COLOR, height=GAME_HEIGHT, width=GAME_WIDTH)
+canvas.pack()
+
+#Updates the window (NOT WINDOWS DON'T WORRY)
+window.update()
+
+#Sets the volume of the munching sound effect to 0.05 so it doesn't demolish your eardrums (thank me later)
+sound_effect.set_volume(0.05)
+
+#Now this steals information about your computers screen
+window_width = window.winfo_width()
+window_height = window.winfo_height()
+screen_width =  window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+
+#Defines x and y with your screen info divided by 2
+x = int((screen_width/2) - (window_width/2))
+y = int((screen_height/2) - (window_height/2))
+
+#I failed geometry at school but this centers the window
+window.geometry(f"{window_width}x{window_height}+{x}+{y}")
+
+#My game is so bad, One Direction CHANGED DIRECTION
+#Jokes aside binds WASD to change direction
+window.bind('<a>', lambda event: change_direction('left'))
+window.bind('<d>', lambda event: change_direction('right'))
+window.bind('<w>', lambda event: change_direction('up'))
+window.bind('<s>', lambda event: change_direction('down'))
+
+#It seems like I had to bind it 2 times because movement wouldn't work if the caps were on lmao
+window.bind('<A>', lambda event: change_direction('left'))
+window.bind('<D>', lambda event: change_direction('right'))
+window.bind('<W>', lambda event: change_direction('up'))
+window.bind('<S>', lambda event: change_direction('down'))
+
+#snake is Snake
+snake = Snake()
+#food is Food
+food = Food()
+
+#call next turn for snake and food
+next_turn(snake, food)
+
+#lööps bröther
+window.mainloop()
